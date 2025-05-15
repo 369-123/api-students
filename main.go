6b9d93a2d1553ce3206ae594e86aeb8dfbcae138
369-123 <Daan369@hotmail.com>
2025-05-15 12:43:13 -0300

    add initial echo route

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
+  "net/http"
+)
+
+func main() {
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // Routes
+  e.GET("/students", getStudents)
+  e.GET("/teachers", getTeachers)
+
+  // Start server
+  e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handler
+func getStudents(c echo.Context) error {
+  return c.String(http.StatusOK, "List of all students")
+}
+
+func getTeachers(c echo.Context) error {
+  return c.String(http.StatusOK, "list of all teachers")
+}
