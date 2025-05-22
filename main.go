7de14f51b761a7d91a7eaec86173e5e9ad7fbc46
369-123 <Daan369@hotmail.com>
2025-05-21 21:35:53 -0300

    Create DB instance in API

--- main.go
+++ main.go
@@ -1,69 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/369-123/api-students/db"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"github.com/369-123/api-students/api"
 )
 
 func main() {
-	// Echo instance
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/students", getStudents)
-	e.POST("/students", createStudents)
-	e.GET("/students/:id", getStudent)
-	e.PUT("/students/:id", updateStudent)
-	e.DELETE("/students/:id", deleteStudent)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
-}
-
-// Handler
-func getStudents(c echo.Context) error {
-  students, err := db.GetStudent()
-  if err != nil {
-    return c.String(http.StatusNotFound, "Failed to get students")
-  }
-
-	return c.JSON(http.StatusOK, students)
-}
-
-func createStudents(c echo.Context) error {
-	student := db.Student{}
-	if err := c.Bind(&student); err != nil {
-		return err
+	server := api.NewServer()
+	server.ConfigureRoutes()
+	
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
 	}
-
-	if err := db.AddStudent(student); err != nil {
-    return c.String(http.StatusInternalServerError, "Error to create student")
-  }
-	return c.String(http.StatusOK, "create student")
-}
-
-func getStudent(c echo.Context) error {
-	id := c.Param("id")
-	getStud := fmt.Sprintf("Get %s student", id)
-	return c.String(http.StatusOK, getStud)
-}
-
-func updateStudent(c echo.Context) error {
-	id := c.Param("id")
-	updateStud := fmt.Sprintf("Get %s student", id)
-	return c.String(http.StatusOK, updateStud)
-}
-
-func deleteStudent(c echo.Context) error {
-	id := c.Param("id")
-	deleteStud := fmt.Sprintf("Get %s student", id)
-	return c.String(http.StatusOK, deleteStud)
 }
